a2557b91f1ebeb1ebc74de89b349308578eb94f2
Petergrac <petergichuke035@gmail.com>
2025-06-08 14:26:50 +0300

    Added some effects to the message

--- src/components/Game(Main Logic).jsx
+++ src/components/Game(Main Logic).jsx
@@ -19,12 +19,12 @@ const getPokemon = async (count = 12) => {
     image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${p.id}.png`,
   }));
 };
-
 export default function Game() {
   const [cards, setCards] = useState([]);
   const [clicked, setClicked] = useState(new Set());
   const [score, setScore] = useState(0);
-  const [bestscore, setBestScore] = useState(0);
+ const [bestscore, setBestScore] = useState(() => Number(localStorage.getItem('bestscore')) || 0);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     getPokemon(12).then(setCards);
@@ -36,38 +36,78 @@ export default function Game() {
       if (score > bestscore) {
         setBestScore(score);
       }
+      setMessage("You Lost");
       setScore(0);
       setClicked(new Set());
     }
     // Logic to update the game
     else {
-      setScore((prev) => prev + 1); // Increment Score
-      setClicked((prev) => new Set(prev).add(id)); // Append the id to the state
-      const shuffled = ShuffleArray(cards); // Shuffle the cards
-      setCards(shuffled); // Update the state with new cards
+      const nextScore = score + 1;
+      if (nextScore >= 12) {
+        setBestScore(nextScore);
+        setMessage("You Won");
+        const shuffled = ShuffleArray(cards);
+        setCards(shuffled);
+        setScore(0);
+        setClicked(new Set());
+      } else {
+        setScore(nextScore);
+        setClicked((prev) => new Set(prev).add(id));
+        const shuffled = ShuffleArray(cards);
+        setCards(shuffled);
+        setMessage("");
+      }
     }
   };
-
+  // Customizing the styles based on the message
+  const scoreClass = score === 12 ? "custom-green" : "";
+  const messageClass =
+    message === "You Won"
+      ? "text-green-400 font-bold"
+      : message === "You Lost"
+      ? "text-red-400 font-bold"
+      : "";
+  // Save the best score to your phone
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      localStorage.setItem("bestscore", bestscore);
+    };
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => window.removeEventListener("beforeunload", handleBeforeUnload);
+  }, [bestscore]);
   return (
     <div className=" bg-teal-600 dark:bg-gray-950 min-h-screen ">
       <header className="flex justify-between p-2 border-2 border-violet-700 dark:border-teal-500 rounded-2xl">
-        <p className="hover:underline decoration-4 decoration-lime-400 dark:decoration-sky-400 text-amber-50 text-sm pt-2 text-center font-game md:pt-8 md:pl-8 md:pb-3 md:text-5xl">
+        <p
+          className={`hover:underline text-[1.2rem] decoration-4 ${scoreClass} decoration-lime-400 dark:decoration-sky-400 text-amber-50 pt-2 text-center font-game md:pt-8 md:pl-8 md:pb-3 md:text-5xl`}
+        >
           Pokemon Memory Game
         </p>
+        <p
+          className={`font-sans font-extrabold items-center pt-2 ${messageClass}`}
+        >
+          {message}
+        </p>
         <div className="flex flex-col text-sm text-white font-sans md:font-game md:text-3xl">
-          <p>Score: {score}</p>
-          <p>Best Score: {bestscore}</p>
+          <p className={`${scoreClass}`}>
+            <span className="text-green-200">Score: </span>
+            {score}
+          </p>
+          <p className={`${scoreClass}`}>
+            <span className="text-green-200">Best Score:</span> {bestscore}
+          </p>
         </div>
       </header>
       <main className="border-violet-800 dark:border-teal-500 rounded-2xl border-2">
         <section className="m-0 flex flex-wrap justify-center p-4">
           {cards.map((card) => (
-            <div key={card.id} onClick={() => handleClick(card.id)}>
-              <img
-                className="w-20 md:w-53 border-rose-800 custom-fuchsia-shadow dark:border-green-500 border-2 rounded-2xl m-1 bg-blue-500/10 hover:bg-blue-500"
-                src={card.image}
-                alt={card.name}
-              />
+            <div
+              key={card.id}
+              onClick={() => handleClick(card.id)}
+              className="w-35 md:w-53 border-rose-800 custom-fuchsia-shadow dark:border-green-500 border-2 rounded-2xl m-1 bg-blue-500/10 hover:bg-blue-500"
+            >
+              <img className="" src={card.image} alt={card.name} />
+              <p className="text-white text-center font-game">{card.name}</p>
             </div>
           ))}
         </section>
